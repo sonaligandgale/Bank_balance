7491668e6bdcd1b1a84f0f96ca1201eb65993d67
sonali gandgalekar <sonaligandgalekar@gmail.com>
2025-10-23 16:05:39 +0530

    save to withdraw-feature

--- Bank_balance.py
+++ Bank_balance.py
@@ -1,9 +1,15 @@
-# Bank Account Balance Program
+
+# Bank Account Balance Program with Withdraw Feature
 initial_balance = float(input("Enter initial balance: ₹"))
 deposit = float(input("Enter deposit amount: ₹"))
 
-new_balance = initial_balance + deposit
-
+balance_after_deposit = initial_balance + deposit
 print("Initial Balance: ₹", initial_balance)
 print("Deposit: ₹", deposit)
-print("New Balance after deposit: ₹", new_balance)
+print("New Balance after deposit: ₹", balance_after_deposit)
+
+withdraw = float(input("Enter withdraw amount: ₹"))
+final_balance = balance_after_deposit - withdraw
+
+print("Withdraw: ₹", withdraw)
+print("Final Balance: ₹", final_balance)
